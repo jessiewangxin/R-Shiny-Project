4cb3aa2057e74d4923e2a3e2b47d6f54c87082b8
Jessie Wang <jessiewang@Jessies-MBP.home>
2020-04-26 13:04:27 -0400

    made formatting updates, included text explanation, forgot to commit my work from saturday

--- server.R
+++ server.R
@@ -7,6 +7,7 @@ function(input,output,session){
                   fontWeight ='bold')
   })
 
+#data used for the interactive map on the map tab
   mapdata <- reactive({
     airbnb %>% 
       mutate(n=1) %>% 
@@ -22,32 +23,25 @@ function(input,output,session){
                         neighbourhood," (",neighbourhood_group,")", sep=""))
   })
     
-   
-  #formatting label for the map 
-  # mapdata$info<- paste("$",as.character(round(mapdata$avg_price,0)),"/night, ",
-  #                   mapdata$neighbourhood," (",mapdata$neighbourhood_group,")", sep="")
-  # 
-  #adding colors for the map
-  boroughcolors <- colorNumeric(palette = "Set1",domain = 0:500)
-  
-  
+  test=mapdata 
+  #adding colors for the map (map tab)
+  boroughcolors <- colorNumeric(palette = "BuPu",domain = 0:500)
+  #boroughcolors <- colorNumeric(palette = "Set1",domain = mapdata$price_bucket)
   
   #MAP 1 SCATTER OF AVERAGE PRICES 
   output$map1 <- renderLeaflet({
-    #maxprice<-7
-  
     mapdata() %>% filter(avg_price<=input$map_pricerange[2] & avg_price>=input$map_pricerange[1]) %>% 
     leaflet() %>% 
       addProviderTiles("CartoDB.Positron") %>% 
       addCircleMarkers(~long, ~lat, 
                        color= ~boroughcolors(avg_price),
                        fillOpacity = 1,
-                       radius=1,
+                       radius=3,
                        label = ~info) %>% 
       addLegend('bottomright', 
                 pal=boroughcolors, 
                 values=~avg_price, 
-                title = "Number of Listings") 
+                title = "Average Price") 
     
   })
   
@@ -55,7 +49,7 @@ function(input,output,session){
   bar_data <- reactive({
     airbnb %>% 
       mutate(n=1) %>% 
-      filter(room_type=="Private room",neighbourhood_group=="Brooklyn") %>% 
+      filter(room_type==input$roomtype2,neighbourhood_group==input$borough2) %>% 
       group_by(neighbourhood,neighbourhood_group) %>% 
       summarise(
         long=mean(longitude),
@@ -71,20 +65,10 @@ function(input,output,session){
       arrange(desc(num_listings))
   })
   
-   # output$lePlot <- renderPlot({
-   #   bar_data()[1:5,] %>% 
-   #      ggplot(aes(x=reorder(neighbourhood,desc(num_listings)),y=num_listings)) + 
-   #      geom_bar(stat='identity', fill = "light blue") + 
-   #      theme_bw() +
-   #      ylab("Average Number of Reviews") +
-   #      xlab("") +
-   #      ggtitle("Top 5 Listed Neighborhoods") +
-   #      theme(plot.title = element_text(hjust = 0.5)) +
-   #      coord_flip()
-   # })
   
     output$leBubblePlot <- renderPlotly({
-      ggplotly(ggplot(bar_data()[1:5,],
+      ggplotly(ggplot(bar_data()[1:input$numoptions,] %>% 
+                 filter(avg_price<=input$pricerange2[2] & avg_price>=input$pricerange2[1]),
                  aes(x=avg_price, y=num_listings, size = avg_reviews, color = neighbourhood,text=text)) +
                  geom_point(alpha=0.7) +
                  scale_size(range = c(5, 15)) +
@@ -92,14 +76,16 @@ function(input,output,session){
                  theme_bw() +
                  xlab("Average Price") + 
                  ylab("Number of Listings") +
-                 ggtitle("TOP 5") +
-                 theme(legend.position="topright",plot.title = element_text(hjust = 0.5)),tooltip="text") 
+                 ggtitle("Most Listed Neighborhoods") +
+                 theme(legend.title=element_blank(),legend.position="topright",plot.title = element_text(hjust = 0.5)),tooltip="text") %>% 
+                 layout(autosize = FALSE, height = 425,width=650)
     })
    
    #MAP 2 select locations  
     output$map2 <- renderLeaflet({
       #maxprice<-7
-      bar_data()[1:5,] %>% 
+      bar_data()[1:input$numoptions,] %>% 
+        filter(avg_price<=input$pricerange2[2] & avg_price>=input$pricerange2[1]) %>% 
         leaflet() %>% 
         addProviderTiles("CartoDB.Positron") %>% 
         addMarkers(~long, ~lat, label = ~neighbourhood) 
--- ui.R
+++ ui.R
@@ -1,21 +1,22 @@
 
 
 dashboardPage(skin='black',
-  dashboardHeader(title='Airbnb Listings in NYC'),
+  dashboardHeader(title='NYC Airbnb Data Project'),
   dashboardSidebar(
-    sidebarUserPanel("Jessie Wang",
-                     image="Jessie1.jpeg"),
     sidebarMenu(
       menuItem('Introduction',tabName= 'introduction',icon=icon('info')),
       menuItem('Map',tabName = 'map',icon=icon('map')),
       menuItem('Where to Stay',tabName = 'analytics',icon=icon('question')),
+      menuItem('Summaries',tabName = 'summaries',icon=icon('adjust')),
       menuItem('Data',tabName = 'data',icon=icon('database')),
       menuItem('About Jessie',tabName= 'about',icon=icon('book')))),
   dashboardBody(
     tabItems(
           tabItem(tabName='introduction',
             HTML('
-            <p><center><b>REPLACE WITH TEXT</b></center></p>
+            <p><center><b><font size="6">Visualizing NYC Airbnb Data</font></b></center></p>
+            <p><center><font size="4">Finding the Best Neighborhoods to Stay in New York</font></center></p>
+            <p><center>by Jessie Wang</center></p>
             <p><center>
             <img src="NewYork2.png", height="400px"    
             style="float:center"/>
@@ -26,8 +27,42 @@ dashboardPage(skin='black',
             (photos taken by Jessie Wang)
             </center></p>
             <p><center>
-            REPLACE WITH TEXT
-            </center></p>')),
+            The purpose of this project is to help users determine the best neighborhood to stay in New York City, by 
+            observing several factors such as: price, borough, property type, number of listings. Below is a summary of the tabs in this Shiny app, please 
+            refer to each respective tab for more detailed information and instructions.
+            </center></p>
+            
+            <center><p>
+          <table style="width: 417px; height: 258px;">
+          <tbody>
+          <tr>
+          <td style="width: 120.671875px;"><u><b>Tab Name</b></u></td>
+          <td style="width: 295.34375px;"><u><b>Description</b></u></td>
+          </tr>
+          <tr>
+          <td style="width: 120.671875px;"><b>Map</b></td>
+          <td style="width: 295.34375px;">Interactive map that displays average price of listings in each neighborhood.</td>
+          </tr>
+          <tr>
+          <td style="width: 120.671875px;"><b>Where to Stay</b></td>
+          <td style="width: 295.34375px;">Presents a maximum of 10 recommended neighborhoods that fit the user input criteria.</td>
+          </tr>
+          <tr>
+          <td style="width: 120.671875px;"><b>Summaries</b></td>
+          <td style="width: 295.34375px;">Other data visualizations and insights.</td>
+          </tr>
+          <tr>
+          <td style="width: 120.671875px;"><b>Data</b></td>
+          <td style="width: 295.34375px;">Data used for project, and source.</td>
+          </tr>
+          <tr>
+          <td style="width: 120.671875px;"><b>About Jessie</b></td>
+          <td style="width: 295.34375px;">About me, includes links to my Github, project code, and contact information.</td>
+          </tr>
+          </tbody>
+          </table>
+          </center></p>
+                 ')),
           #INTERACTIVE MAP TAB
           tabItem(tabName='map',
                   #leaflet map
@@ -57,7 +92,11 @@ dashboardPage(skin='black',
                          '<br></br>
                          <b>Instructions: </b>
                          <br></br>
-                         INSERT TEXT HERE
+                         Please specify your preferences using the slide and drop down menus. 
+                         If you receive an error, please try another set of inputs.
+                         <br></br>                         
+                         Light blue markers on the map indicate cheaper neighborhoods (on average), and dark purple ones 
+                         indicate the most expensive. Hover over a circle to view the borough name and average price per night (per listing).
                          <br></br>
                          <br></br>
                          '))
@@ -69,34 +108,71 @@ dashboardPage(skin='black',
                   
                   #plots
                   fluidRow(
-                    column(4,"TEXT HERE"),
-                  # column(6,plotOutput("lePlot")),
+                    column(4,
+                           "Please specify your preferences using the options below. If you receive an error, please try 
+                           another set of inputs.",
+                           HTML('<br></br>'),
+                           sliderInput("numoptions",
+                                       "Number of Options:",
+                                       min = 1,
+                                       max = 10,
+                                       value = 5),
+                           sliderInput("pricerange2",
+                                       "Price Range:",
+                                       min = 32,
+                                       max = 500,
+                                       value = c(75,300)),
+                           selectizeInput(inputId ="roomtype2",
+                                          label = "Room Type",
+                                          choices = unique (airbnb$room_type)),
+                           selectizeInput(inputId ="borough2",
+                                          label = "Borough",
+                                          choices = unique (airbnb$neighbourhood_group))
+                           
+                           ),
                   column(8,plotlyOutput("leBubblePlot"))),
                   HTML('<br></br>'),
                   leafletOutput("map2"),
+                  HTML('<br></br>'),
+                  "Note: Top 5 (or as specified) number of recommended neighborhoods
+                  displays those with the highest number of listings that
+                  satisfy the user input criteria. The larger bubbles on the plot reflect higher numbers of 
+                  reviews on average - indicating more bookings and higher popularity. ",
                   HTML('
                          <br></br>
                          <br></br> 
                        ')),
-          
+          tabItem(tabName = 'summaries'),
             #DATA TAB
           tabItem(tabName='data',
-                  fluidRow(box(DT::dataTableOutput("table"), width = 20))),
+                  HTML('
+                     <a href="http://insideairbnb.com/get-the-data.html">Data Source</a>
+                     <br></br> 
+                       '),
+                  
+                  fluidRow(box(DT::dataTableOutput("table"), width = 20)),
+                  HTML('
+                     <br></br> 
+                     <br></br> 
+                       ')),
             
           #JESSIES BIO
           tabItem(tabName='about', 
                   
             fluidRow(
-              column(6,HTML('<p>Hello,</p>
-                              <p>I hope you enjoyed browsing my R Shiny App project. </p>
+              column(6,HTML('
+                              <p>I hope you enjoyed browsing my R Shiny project. </p>
                               <p>A little about myself, I studied Actuarial Science at the University of California, Santa Barbara. 
-                              Upon graduation, I worked as an Actuary in the healthcare industry for several years, 
-                              with an array of experience in: data analytics, financial reporting, reserving and pricing. </p>
-                              <p>I became interested in Data Science, as I have always had a passion for Statistics and technology. I also
-                              like the opportunity to combine my analytical skills with my creative instinct. Aside from Data Science,
-                              my interests include traveling, photography, and cooking - I am always trying new recipes.</p>
+                              Upon graduation, I started my career as an Actuary in the healthcare industry, where I had the opportunity 
+                              to work on various projects utilizing healthcare data. 
+                              </p>
+                              <p>I became interested in data science as there are many ways to use of data creatively as a form 
+                              storytelling. Aside from data science,
+                              my interests include traveling, photography, and cooking - I am always trying new recipes!</p>
                               <p><a href="https://www.linkedin.com/in/jessie-wang-5b82b980/">LinkedIn</a></p>
                               <p><a href="https://github.com/jessiewangxin/">Github</a></p>
+                              <p><a href="https://github.com/jessiewangxin/R-Shiny-Project">R Shiny Project Code</a></p>
+                              <p>E-mail: jessiewangemail@gmail.com</p>
                               <p><br></p>')),
               column(6,
               #PERSONAL IMAGE LINK
